68b952e1418ba050662d33dcfee39745ea17d4a7
Daiana Brancati <brancatisanodaianagomez4@gmail.com>
2025-05-02 12:39:25 -0300

    Primer commit

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { AppComponent } from '../../app.component';
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [AppComponent],
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+
+}
